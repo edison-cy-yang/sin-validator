d128c52c6dfc7dfa1592ea3961f87f3d2cd2f568
Edison Yang <edisonyang@Edisons-MacBook-Pro.local>
2024-08-20 17:27:55 -0700

    some daily UI and styling

--- app/components/SinValidator.tsx
+++ app/components/SinValidator.tsx
@@ -1,7 +1,27 @@
-import React from "react"
+"use client"
+import React, { useState } from "react"
 
 const SinValidator = () => {
-  return <div>SinValidator</div>
+  const [sin, setSIN] = useState("")
+  const [isValid, setIsValid] = useState(false)
+
+  const handleSINChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSIN = event.target.value
+    setSIN(newSIN)
+  }
+
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="text-lg text-neutral-600">SIN Validator</div>
+      <input
+        className="input input-primary input-bordered w-full max-w-xs"
+        type="text"
+        value={sin}
+        onChange={handleSINChange}
+        placeholder="Please enter the SIN number"
+      ></input>
+    </div>
+  )
 }
 
 export default SinValidator
